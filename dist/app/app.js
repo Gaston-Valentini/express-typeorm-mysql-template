e5dc3d946617c119c82ef762802b97448d20111a
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-10-22 19:08:53 +0200

    Add: Environment variables and settings configured.

--- dist/app/app.js
+++ dist/app/app.js
@@ -6,8 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // Imports
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const routerRouteOne_1 = __importDefault(require("../router/routerRouteOne"));
 const routerRouteTwo_1 = __importDefault(require("../router/routerRouteTwo"));
+// Settings
+app.set("PORT", process.env.PORT || 4000);
+app.set("HOST", process.env.HOST);
+app.set("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD);
 // Router
 app.use("/routeOne", routerRouteOne_1.default);
 app.use("/routeTwo", routerRouteTwo_1.default);
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,16 @@
 // Imports
 import express from "express"
 const app = express()
+import dotenv from "dotenv"
+dotenv.config()
 
 import routerRouteOne from "../router/routerRouteOne"
 import routerRouteTwo from "../router/routerRouteTwo"
+
+// Settings
+app.set("PORT", process.env.PORT || 4000)
+app.set("HOST", process.env.HOST)
+app.set("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD)
 
 // Router
 app.use("/routeOne", routerRouteOne)
